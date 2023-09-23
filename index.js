0798ee7f6ee766bc5a8c567e9e45002d69180587
emmanuel-eshun <145821368+emmanuel-eshun@users.noreply.github.com>
2023-09-23 03:42:52 +0000

    final ship

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 // Define a route
 app.get("/", (req, res) => {
+compile();
   res.send("Hello, Express!");
 });
 
@@ -17,10 +18,9 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-Schedule a job to run every day at a specific time (e.g., 8:00 am)
-cron.schedule("*/30 * * * * *", () => {
-  // compile();
-  console.log("Daily cron job is running!");
-});
+// Schedule a job to run every day at a specific time (e.g., 8:00 am)
+// cron.schedule("*/30 * * * * *", () => {
+//   // compile();
+//   console.log("Daily cron job is running!");
+// });
 
-// compile();
