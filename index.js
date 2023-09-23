944e1d53ec0f6b486a9c18839c7c65a6e856335c
Emmanuel Eshun <emmanuel.eshun@amalitech.com>
2023-09-23 12:33:37 +0000

    new

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ app.listen(port, () => {
 });
 
 // Schedule a job to run every day at a specific time (e.g., 8:00 am)
-// cron.schedule("*/30 * * * * *", () => {
+// cron.schedule("0 8 * * *", () => {
+//   compile();
 //   console.log("Daily cron job is running!");
 // });
-
-  compile();
--- scrape.js
+++ scrape.js
@@ -17,33 +17,37 @@ const url2 =
   "https://www.amazon.com/PlayStation-5-Console-CFI-1215A01X/dp/B0BCNKKZ91/ref=sr_1_1?keywords=ps5&qid=1695414457&sr=8-1&th=1";
 
 async function scrape(url) {
-  const product = { name: "", price: "", link: "" };
-  // Fetcg Data
-  const { data } = await axios.get(url).catch((error) => {
-    sendWhatsAppMsg("Something Went Wrong");
-    console.error("Error");
-  });
+  try {
+    const product = { name: "", price: "", link: "" };
+    // Fetcg Data
+    const { data } = await axios.get(url).catch((error) => {
+      console.error("Error");
+    });
+    //   Load up the html
+    const $ = cheerio.load(data);
+    const item = $("div#ppd");
 
-  //   Load up the html
-  const $ = cheerio.load(data);
-  const item = $("div#ppd");
+    // //   Extract data
+    product.name = $(item).find("h1 span#productTitle").text().trim();
+    product.price = $(item).find("span .a-offscreen").first().text();
+    product.link = url;
 
-  //   Extract data
-  product.name = $(item).find("h1 span#productTitle").text().trim();
-  product.price = $(item).find("span .a-offscreen").first().text();
-  product.link = url;
-
-  return product;
-
-  //   Send WhatsApp Message
+    return product;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function compile() {
-  const d1 = await scrape(url1);
-  const d2 = await scrape(url2);
-  const msg = `The Current price of ${d1.name}🎮 is ${d1.price}🔥 and ${d2.name}🎮 is ${d2.price}🔥 as at ${formattedDate}. Follow the link below to buy on Amazon ${d2.link}`;
-
-  sendWhatsAppMsg(msg);
+  try {
+    const d1 = await scrape(url1);
+    const d2 = await scrape(url2);
+    const msg = `The Current price of ${d1.name}🎮 is ${d1.price}🔥 and ${d2.name}🎮 is ${d2.price}🔥 as at ${formattedDate}. Follow the link below to buy on Amazon ${d2.link}`;
+    // // Send WhatsAppMessage
+    sendWhatsAppMsg(msg);
+  } catch (error) {
+    sendWhatsAppMsg("Something went wrong sending message");
+  }
 }
 
 module.exports = { compile };
