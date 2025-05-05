fe0112e0f95ecf8b004afaaabb85d45897cb1528
Emmanuel Eshun <emmanuel.eshun@amalitech.com>
2025-05-05 11:02:53 +0000

    Test creds

--- Jenkinsfile
+++ Jenkinsfile
@@ -13,5 +13,17 @@ pipeline {
       }
     }
 
+    stage('Smoke Test') {
+      steps {
+          withCredentials([string(credentialsId: 'payroll_backend_staging_url', variable: 'payroll_backend_staging_url')]) {
+              sh 'apt-get update'
+              sh 'apt-get install jq -y'
+              sh 'chmod +x ./scripts/smoke.sh'
+              // sh "PAYROLL_BACKEND_URL=$payroll_backend_staging_url ./scripts/smoke.sh"
+              sh './scripts/smoke.sh'
+          }
+      }
+  }
+
   }
 }
---
+++ smoke.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Endpoint: ${payroll_backend_staging_url}"
+PAYROLL_BACKEND_URL=$payroll_backend_staging_url
+
+echo PAYROLL_BACKEND_URL
+
+echo "Something else"
+
+# APPLICATION_HEALTH=$(curl -X POST -H "Content-Type: application/json" -d '{"query": "query {payrollHealthCheck}"}' "${payroll_backend_staging_url}" | jq -r '.data.payrollHealthCheck')
+# DATABASE_HEALTH=$(curl -X POST -H "Content-Type: application/json" -d '{"query": "query {payrollDBCheck}"}' "${payroll_backend_staging_url}" | jq -r '.data.payrollDBCheck')
+
+# if [[ "$APPLICATION_HEALTH" == "OK" && "$DATABASE_HEALTH" == "OK" ]]; then
+#     echo "Health checks passed:"
+#     echo "Application Health: $APPLICATION_HEALTH"
+#     echo "Database Health: $DATABASE_HEALTH"
+# else
+#     echo "Health checks failed:"
+#     echo "Application Health: $APPLICATION_HEALTH"
+#     echo "Database Health: $DATABASE_HEALTH"
+#     exit 1
+# fi
